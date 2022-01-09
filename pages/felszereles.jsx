9b3c066115baaab0546ab30eedd5bc02ee39af80
fbalazs930 <fbalazs930@gmail.com>
2022-01-10 00:15:20 +0100

    jpg -> png

--- pages/felszereles.jsx
+++ pages/felszereles.jsx
@@ -32,49 +32,49 @@ export default function Equipment() {
                 <div className="item">
                     <div>Macbook Pro 13” 2019</div>
                     <div className="img">
-                        <Image alt="Macbook Pro 13 2019" layout="fill" src="https://i.postimg.cc/7P5pJ29v/macbook.jpg" />
+                        <Image alt="Macbook Pro 13 2019" layout="fill" src="https://i.postimg.cc/SQvzMMy8/image-removebg-preview-1.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>2db teremszinező PAR lámpa</div>
                     <div className="img">
-                        <Image alt="PAR lampa" layout="fill" src="https://i.postimg.cc/vHk86yfh/led-teremszinezo.jpg" />
+                        <Image alt="PAR lampa" layout="fill" src="https://i.postimg.cc/mD0zKvpj/image-removebg-preview-2.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>2db Cameo Hydrabeam 1000 RGBW</div>
                     <div className="img">
-                        <Image alt="Cameo Hydrabeam 1000 RGBW" layout="fill" src="https://i.postimg.cc/pTT3ZhtF/CAmeo-hydrabeam-1000.jpg" />
+                        <Image alt="Cameo Hydrabeam 1000 RGBW" layout="fill" src="https://i.postimg.cc/63GTVH6v/image-removebg-preview-3.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>2db EUROLITE - LED TMH-13 Moving-Head Spot</div>
                     <div className="img">
-                        <Image alt="EUROLITE - LED TMH-13 Moving-Head Spot" layout="fill" src="https://i.postimg.cc/Px0hvz8x/Eurolite-led-tmh-13.png" />
+                        <Image alt="EUROLITE - LED TMH-13 Moving-Head Spot" layout="fill" src="https://i.postimg.cc/d0h0vWyx/image-removebg-preview-4.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>EUROLITE - LED BAR-6 QCL RGBW Bar</div>
                     <div className="img">
-                        <Image alt="EUROLITE - LED BAR-6 QCL RGBW Bar" layout="fill" src="https://i.postimg.cc/D0Vkwvt4/Eurolite-led-bar-6.png" />
+                        <Image alt="EUROLITE - LED BAR-6 QCL RGBW Bar" layout="fill" src="https://i.postimg.cc/sXF3z3yM/image-removebg-preview-5.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>EUROLITE - LED Party UV Bar 9</div>
                     <div className="img">
-                        <Image alt="EUROLITE - LED Party UV Bar 9" layout="fill" src="https://i.postimg.cc/9FVK8nV2/UV.png" />
+                        <Image alt="EUROLITE - LED Party UV Bar 9" layout="fill" src="https://i.postimg.cc/Hnzmb5MM/image-removebg-preview-6.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>Involtight FOG1200</div>
                     <div className="img">
-                        <Image alt="Involtight FOG1200" layout="fill" src="https://i.postimg.cc/DzWM4VQr/involight-fog1200.jpg" />
+                        <Image alt="Involtight FOG1200" layout="fill" src="https://i.postimg.cc/rwKTtDwd/image-removebg-preview-7.png" />
                     </div>
                 </div>
                 <div className="item">
                     <div>Cerebellum digi 128M fényvezérlő</div>
                     <div className="img">
-                        <Image alt="Cerebellum digi 128M" layout="fill" src="https://i.postimg.cc/HkNSgZGV/Cerebellum-Digi-128-M-m.jpg" />
+                        <Image alt="Cerebellum digi 128M" layout="fill" src="https://i.postimg.cc/fLs15yDf/image-removebg-preview-8.png" />
                     </div>
                 </div>
             </div>
